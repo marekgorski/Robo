94d4fd8432c1c4fbe14c36dee034f59442c8101b
Alienware <Alienware@Alienware-PC>
2013-01-02 12:17:12 +1100

    New Contender added

---
+++ RoboGame/src/com/eatthatgame/robo/contenders/Adam.java
@@ -0,0 +1,14 @@
+package com.eatthatgame.robo.contenders;
+
+import com.eatthatgame.robo.Contender;
+
+public class Adam extends Contender {
+
+    public Adam() {
+        this.name = "Adam";
+        this.age = 47;
+        this.damage = 33;
+    }
+
+}
+
