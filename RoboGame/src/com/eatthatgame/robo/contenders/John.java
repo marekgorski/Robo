0f80a9d2df9fde7775ea3f990c8a7b625a3dd977
John <>
2013-01-03 08:43:55 +1100

    Made my own contender. Name = "Blitz" Age = "6" Damage = "25"

---
+++ RoboGame/src/com/eatthatgame/robo/contenders/John.java
@@ -0,0 +1,14 @@
+package com.eatthatgame.robo.contenders;
+
+import com.eatthatgame.robo.Contender;
+
+public class John extends Contender 
+{
+            
+    public John() 
+    {
+        this.name = "Blitz";
+        this.age = 6;
+        this.damage = 25; // HOOK CITY *****, HOOK CITY!
+    }
+}
