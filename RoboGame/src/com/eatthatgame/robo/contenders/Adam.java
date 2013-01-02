421720f22193b4ab2be79839302470fd21f8bfa5
Duderz <ada.me@live.com>
2013-01-02 12:26:25 +1100

    Update RoboGame/src/com/eatthatgame/robo/contenders/Adam.java

--- RoboGame/src/com/eatthatgame/robo/contenders/Adam.java
+++ RoboGame/src/com/eatthatgame/robo/contenders/Adam.java
@@ -5,7 +5,7 @@ import com.eatthatgame.robo.Contender;
 public class Adam extends Contender {
 
     public Adam() {
-        this.name = "Adams";
+        this.name = "Adam's";
         this.age = 1;
         this.damage = 33;
     }
