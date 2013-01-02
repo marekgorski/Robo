38c784838439246fac0af1654c5471499959c822
Nathan <Nathan@Nathan-PC>
2013-01-02 12:07:34 +1100

    Adding my own contender (Max the robot)

---
+++ RoboGame/src/com/eatthatgame/robo/contenders/NathanRobo.java
@@ -0,0 +1,13 @@
+package com.eatthatgame.robo.contenders;
+
+import com.eatthatgame.robo.Contender;
+
+public class NathanRobo extends Contender {
+
+    public NathanRobo() {
+        this.name = "Max";
+        this.age = 6;
+        this.damage = 42;
+    }
+
+}
