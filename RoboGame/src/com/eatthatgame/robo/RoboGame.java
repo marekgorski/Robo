d73cc3a84602025898417f3796826608560356f1
Mark <>
2013-01-04 20:40:37 +1100

    Step 2 of refactoring the fight mechanic - moved fighting into new League class, passing it the two contenders

--- RoboGame/src/com/eatthatgame/robo/RoboGame.java
+++ RoboGame/src/com/eatthatgame/robo/RoboGame.java
@@ -5,22 +5,12 @@ import com.eatthatgame.robo.contenders.*;
 public class RoboGame {
 
     public static void main(String[] args) {
+        
         Contender contenderA = new Adam();
         Contender contenderB = new JayGundam();
 
-        System.out.println(contenderA.name + " vs. " + contenderB.name);
-
-        while((contenderA.health > 0) && (contenderB.health > 0)) {
-            contenderA.health -= contenderB.damage;
-            contenderB.health -= contenderA.damage;
-            System.out.println(contenderA.name + " health: " + contenderA.health);
-            System.out.println(contenderB.name + " health: " + contenderB.health);
-        }
-        if(contenderA.health > contenderB.health) {
-            System.out.println(contenderA.name + "Robo wins!");
-        } else {
-            System.out.println(contenderB.name + "Robo wins!");
-        }
+        League league = new League(contenderA, contenderB);
+        
     }
 
 }
