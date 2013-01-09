1b0c802bd5eccc776a8312d434214e0eed87c20d
Mark <>
2013-01-10 10:42:09 +1100

    manual update of Nathan's Robo class changes

--- RoboGame/src/com/eatthatgame/robo/contenders/NathanRobo.java
+++ RoboGame/src/com/eatthatgame/robo/contenders/NathanRobo.java
@@ -7,7 +7,32 @@ public class NathanRobo extends Contender {
     public NathanRobo() {
         this.name = "Max";
         this.age = 6;
-        this.attackPower = 42;
+        this.attackPower = 23;
+        this.defensePower = 13;
+          }
+    
+    @Override
+    public void AI(){
+        int RoboHealth = getHealth();
+        
+        if( RoboHealth == 100) {
+            System.out.println(name + " Attacks!");
+            attack = true;
+            
+        if( RoboHealth > 75) {
+            System.out.println(name + " Defends!");
+            attack = true;
+            
+        }
+        if( RoboHealth > 45 ) {
+            System.out.println(name + " Attacks!");
+            attack = true;
+            
+        }
+        } else {
+            System.out.println(name + " Defends!");
+            attack = false;
+        }
     }
 
 }
